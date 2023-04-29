12efed7ffadacc356e46514d562ff9abed25f66d
DaFont <danielfontsuarez@gmail.com>
2023-04-29 15:31:16 +0200

    Add Spring Boot skeleton

---
+++ src/test/java/com/ciber4/calculator4/Calculator4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber4.calculator4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
